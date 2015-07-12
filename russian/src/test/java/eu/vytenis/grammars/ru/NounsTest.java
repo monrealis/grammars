c347c28b33f54d3da04892f193a15db063c2ceae
Vytenis Bivainis <monrealis@gmail.com>
2015-07-13 00:30:56 +0300

    Add feminine singular.

--- russian/src/test/java/eu/vytenis/grammars/ru/NounsTest.java
+++ russian/src/test/java/eu/vytenis/grammars/ru/NounsTest.java
@@ -11,11 +11,17 @@ import org.junit.Test;
 public class NounsTest {
 	@Test
 	public void table() {
-		assertEquals(asList("стол", "стол", "стола", "столу", "столом", "столе"), decline("стол"));
+		assertEquals(asList("стол", "стол", "стола", "столу", "столом", "столе"), declineSingular("стол"));
 		assertEquals(asList("столы", "столы", "столов", "столам", "столами", "столах"), declinePlural("стол"));
 	}
 
-	private List<String> decline(String word) {
+	@Test
+	public void fish() {
+		List<String> singular = asList("рыьа", "рыьу", "рыьы", "рыье", "рыьои", "рыье");
+		assertEquals(singular, declineSingularFeminine("рыьа"));
+	}
+
+	private List<String> declineSingular(String word) {
 		List<String> r = new ArrayList<String>();
 		r.add(word + "");
 		r.add(word + "");
@@ -34,6 +40,18 @@ public class NounsTest {
 		r.add(word + "ам");
 		r.add(word + "ами");
 		r.add(word + "ах");
+		return r;
+	}
+
+	private List<String> declineSingularFeminine(String word) {
+		List<String> r = new ArrayList<String>();
+		String stem = word.substring(0, word.length() - 1);
+		r.add(stem + "а");
+		r.add(stem + "у");
+		r.add(stem + "ы");
+		r.add(stem + "е");
+		r.add(stem + "ои");
+		r.add(stem + "е");
 		return r;
 	}
 }
