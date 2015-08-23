946432b9ff86ffd9b83abc9039f6d8c88e46a558
Vytenis Bivainis <monrealis@gmail.com>
2015-08-24 00:12:35 +0300

    Add phrases to test.

--- german/src/test/java/eu/vytenis/grammars/de/DeclinationTest.java
+++ german/src/test/java/eu/vytenis/grammars/de/DeclinationTest.java
@@ -1,5 +1,34 @@
 package eu.vytenis.grammars.de;
 
-public class DeclinationTest {
+import java.util.ArrayList;
+import java.util.List;
 
+public class DeclinationTest {
+	private static List<String> texts = new ArrayList<String>();
+	static {
+		texts.add("der neue Mantel");
+		texts.add("des neuen Mantels");
+		texts.add("dem neuen Mantel");
+		texts.add("den neuen Mantel");
+		texts.add("ein neuer Mantel");
+		texts.add("eines neuen Mantel");
+		texts.add("einem neuen Mantel");
+		texts.add("einen neuen Mantel");
+		texts.add("die neue Bluse");
+		texts.add("der neuen Bluse");
+		texts.add("der neuen Bluse");
+		texts.add("die neue Bluse");
+		texts.add("das neue Hemd");
+		texts.add("des neuen Hemdes");
+		texts.add("dem neuen Hemd");
+		texts.add("das neue Hemd");
+		texts.add("ein neues Hemd");
+		texts.add("eines neuen Hemdes");
+		texts.add("einem neuen Hemd");
+		texts.add("ein neues Hemd");
+		texts.add("eine neue Bluse");
+		texts.add("einer neuen Bluse");
+		texts.add("einer neuen Bluse");
+		texts.add("eine neue Bluse");
+	}
 }
