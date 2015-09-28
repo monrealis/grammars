eae63bbaf5f9155f77621a25dc16d9a421c8e10c
Vytenis Bivainis <monrealis@gmail.com>
2015-09-29 00:20:35 +0300

    Add BestimmteArtikelForm.

---
+++ german/src/main/java/eu/vytenis/grammars/de/BestimmteArtikelForm.java
@@ -0,0 +1,51 @@
+package eu.vytenis.grammars.de;
+
+import static java.util.Arrays.asList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import lombok.Getter;
+
+public class BestimmteArtikelForm {
+	private static Map<List<Object>, String> strings = createValues();
+
+	private static Map<List<Object>, String> createValues() {
+		Map<List<Object>, String> r = new HashMap<List<Object>, String>();
+		r.put(key(BestimmteArtikel.Der, Kasus.Nominativ), "der");
+		r.put(key(BestimmteArtikel.Der, Kasus.Genitiv), "des");
+		r.put(key(BestimmteArtikel.Der, Kasus.Dativ), "dem");
+		r.put(key(BestimmteArtikel.Der, Kasus.Akkusativ), "den");
+		r.put(key(BestimmteArtikel.Die, Kasus.Nominativ), "die");
+		r.put(key(BestimmteArtikel.Die, Kasus.Genitiv), "der");
+		r.put(key(BestimmteArtikel.Die, Kasus.Dativ), "der");
+		r.put(key(BestimmteArtikel.Die, Kasus.Akkusativ), "die");
+		r.put(key(BestimmteArtikel.Das, Kasus.Nominativ), "das");
+		r.put(key(BestimmteArtikel.Das, Kasus.Genitiv), "des");
+		r.put(key(BestimmteArtikel.Das, Kasus.Dativ), "dem");
+		r.put(key(BestimmteArtikel.Das, Kasus.Akkusativ), "das");
+		return r;
+	}
+
+	private static List<Object> key(BestimmteArtikel b, Kasus k) {
+		return asList((Object) b, k);
+	}
+
+	@Getter
+	private final BestimmteArtikel artikel;
+	@Getter
+	private final Kasus kasus;
+
+	public BestimmteArtikelForm(BestimmteArtikel artikel, Kasus kasus) {
+		this.artikel = artikel;
+		this.kasus = kasus;
+	}
+
+	@Override
+	public String toString() {
+		if (strings.get(key(artikel, kasus)) == null)
+			return "";
+		return strings.get(key(artikel, kasus));
+	}
+}
---
+++ german/src/test/java/eu/vytenis/grammars/de/BestimmteArtikelFormTest.java
@@ -0,0 +1,35 @@
+package eu.vytenis.grammars.de;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class BestimmteArtikelFormTest {
+	@Test
+	public void mannlich() {
+		assertEquals("der", toString(BestimmteArtikel.Der, Kasus.Nominativ));
+		assertEquals("des", toString(BestimmteArtikel.Der, Kasus.Genitiv));
+		assertEquals("dem", toString(BestimmteArtikel.Der, Kasus.Dativ));
+		assertEquals("den", toString(BestimmteArtikel.Der, Kasus.Akkusativ));
+	}
+
+	@Test
+	public void weiblich() {
+		assertEquals("die", toString(BestimmteArtikel.Die, Kasus.Nominativ));
+		assertEquals("der", toString(BestimmteArtikel.Die, Kasus.Genitiv));
+		assertEquals("der", toString(BestimmteArtikel.Die, Kasus.Dativ));
+		assertEquals("die", toString(BestimmteArtikel.Die, Kasus.Akkusativ));
+	}
+
+	@Test
+	public void neutral() {
+		assertEquals("das", toString(BestimmteArtikel.Das, Kasus.Nominativ));
+		assertEquals("des", toString(BestimmteArtikel.Das, Kasus.Genitiv));
+		assertEquals("dem", toString(BestimmteArtikel.Das, Kasus.Dativ));
+		assertEquals("das", toString(BestimmteArtikel.Das, Kasus.Akkusativ));
+	}
+
+	private String toString(BestimmteArtikel d, Kasus k) {
+		return new BestimmteArtikelForm(d, k).toString();
+	}
+}
