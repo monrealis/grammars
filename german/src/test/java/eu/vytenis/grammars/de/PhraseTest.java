a53dc03d9d2984ac36a387324850722190890aa6
Vytenis Bivainis <monrealis@gmail.com>
2015-10-10 23:09:49 +0300

    Test Phrase.toString().

---
+++ german/src/test/java/eu/vytenis/grammars/de/PhraseTest.java
@@ -0,0 +1,17 @@
+package eu.vytenis.grammars.de;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class PhraseTest {
+	@Test
+	public void toString_returnsString() {
+		assertEquals("der Tisch", derTisch().toString());
+	}
+
+	private Phrase derTisch() {
+		return new Phrase(BestimmteArtikel.Der, new Substantiv("Tisch"));
+	}
+
+}
