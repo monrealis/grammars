77c357dfbe87dfabb92abd0f2538aee3cd3deb6f
Vytenis Bivainis <monrealis@gmail.com>
2015-10-26 22:17:13 +0200

    Add ending to Substantiv.

--- german/src/main/java/eu/vytenis/grammars/de/Substantiv.java
+++ german/src/main/java/eu/vytenis/grammars/de/Substantiv.java
@@ -4,14 +4,25 @@ import lombok.Getter;
 
 public class Substantiv implements Part {
 	@Getter
-	private final String word;
+	private final String substantiv;
+	@Getter
+	private final String ending;
 
-	public Substantiv(String word) {
-		this.word = word;
+	public Substantiv(String substantiv) {
+		this(substantiv, "");
+	}
+
+	public Substantiv(String substantiv, String ending) {
+		this.substantiv = substantiv;
+		this.ending = ending;
+	}
+
+	public Adjektiv withEnding(String ending) {
+		return new Adjektiv(substantiv, ending);
 	}
 
 	@Override
 	public String toString() {
-		return word;
+		return new Wort(substantiv, ending).toString();
 	}
 }
