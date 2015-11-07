77fe7a62fc09048e8be2c2c492ed2af81f64a8fd
Vytenis Bivainis <monrealis@gmail.com>
2015-11-08 00:12:13 +0200

    Add features.

--- german/src/main/java/eu/vytenis/grammars/de/Substantiv.java
+++ german/src/main/java/eu/vytenis/grammars/de/Substantiv.java
@@ -1,5 +1,10 @@
 package eu.vytenis.grammars.de;
 
+import static java.util.Collections.unmodifiableSet;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import lombok.Getter;
 
 public class Substantiv implements Part {
@@ -7,6 +12,7 @@ public class Substantiv implements Part {
 	private final String substantiv;
 	@Getter
 	private final String ending;
+	private final Set<SubstantivFeature> features = new HashSet<>();
 
 	public Substantiv(String substantiv) {
 		this(substantiv, "");
@@ -19,6 +25,16 @@ public class Substantiv implements Part {
 
 	public Substantiv withEnding(String ending) {
 		return new Substantiv(substantiv, ending);
+	}
+
+	public Substantiv withFeature(Set<SubstantivFeature> features) {
+		// TODO make immutable
+		features.addAll(features);
+		return this;
+	}
+	
+	public Set<SubstantivFeature> getFeatures() {
+		return unmodifiableSet(features);
 	}
 
 	@Override
---
+++ german/src/main/java/eu/vytenis/grammars/de/SubstantivFeature.java
@@ -0,0 +1,6 @@
+package eu.vytenis.grammars.de;
+
+public enum SubstantivFeature {
+	IsMannlich, IsWeiblich, IsNeutral;
+
+}
